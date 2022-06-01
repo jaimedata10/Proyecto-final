14e7a00b2ecce13973ea8cb2d2bcb08afc29a761
Alvaro Perez <alvargta999@gmail.com>
2022-06-01 19:29:19 +0200

    Commit arreglo calculadora

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,42 +1,76 @@
-function myMedia( x,  y, z, cont) {
-var x, y, z, sumaTotal,cont;
-sumaTotal =(parseFloat(x)+parseFloat(y)+parseFloat(z))/3;
+
+function myMedia( Price, Entrance , Months, tae, cont) {
+var Price, Entrance, Months, sumaTotal, sumaDivide, restaPriceEntrance, cont;
+var tae = 0.07;
+
+console.log(Price);
+console.log(Entrance);
+console.log(Months);
+console.log(tae);
+
+restaPriceEntrance = (parseFloat(Price)- parseFloat(Entrance));
+
+sumaTotal =( ((parseFloat(Price)-parseFloat(Entrance))*tae) ).toFixed(2);
+
+
+console.log(sumaTotal);
+
+console.log('lo de abajo es lo de divide')
+
+sumaDivide = ( ( parseFloat(restaPriceEntrance) + parseFloat(sumaTotal) ) / parseFloat(Months) ).toFixed(2);
+
+
+console.log(sumaDivide);
+console.log(sumaTotal);
 
 if(isNaN(sumaTotal)){
 sumaTotal= '';
 }
 
-if(x==''||y==''||z==''){
+if(isNaN(sumaDivide)){
+    sumaDivide= '';
+    }
+
+/* 
+if(Price==''||Entrance==''||Months==''){
 alert('Rellena todos los campos')
 }
 
-if((x<0||x>10)||(y<0||y>10)||(z<0||z>10)){
-alert('Los numeros tienes que estar entre el 0 y el 10');
+if((Price<0||Price>90000)||(Entrance<0||Entrance>90000)||(Months<0||Months>90000)){
+alert('Los numeros tienes que estar entre el 0 y el 90000');
 return '';
 }
+*/
 
 
-if(cont==1){
-document.getElementById("promedio1").innerHTML= sumaTotal.toFixed(2);
 
-}
-else if(cont==2){
-document.getElementById("promedio2").innerHTML = sumaTotal.toFixed(2);
 
-}
-else if(cont==3){
-document.getElementById("promedio3").innerHTML = sumaTotal.toFixed(2);
 
-}
-else if(cont==4){
-document.getElementById("promedio4").innerHTML = sumaTotal.toFixed(2);
 
-}
-else{
-document.getElementById("promedio5").innerHTML = sumaTotal.toFixed(2);
 
-}
+//esto es una prueba 
 
+
+        if ( Price ==="" || Entrance ==="" || Months ==="" ) { 
+            document.getElementById("demo").innerHTML = "Todos los campos son obligatorios";
+            return false
+        }
+        else { 
+            document.getElementById("demo").innerHTML = "";
+           
+            
+            if(cont==1){
+
+                document.getElementById("resultadoFinal").innerHTML = sumaDivide;
+                document.getElementById("Prize").innerHTML= Price;
+                document.getElementById("Entrance").innerHTML = Entrance;
+                document.getElementById("Months").innerHTML = Months;
+                
+            }
+
+            return false
+        }
+        
 
 }
     
