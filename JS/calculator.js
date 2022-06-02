3420056a1f7cf9ce119dee65f6e8799d150d6d9a
Alvaro Perez <alvargta999@gmail.com>
2022-06-02 20:00:17 +0200

    Enlaces y calculadora

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,77 +1,56 @@
 
-function myMedia( Price, Entrance , Months, tae, cont) {
-var Price, Entrance, Months, sumaTotal, sumaDivide, restaPriceEntrance, cont;
-var tae = 0.07;
+function myMedia(Price, Entrance, Months, tae, cont) {
+    var Price, Entrance, Months, sumaTotal, sumaDivide, restaPriceEntrance, cont;
+    var tae = 0.07;
 
-console.log(Price);
-console.log(Entrance);
-console.log(Months);
-console.log(tae);
+    restaPriceEntrance = (parseFloat(Price) - parseFloat(Entrance));
+    sumaTotal = (((parseFloat(Price) - parseFloat(Entrance)) * tae)).toFixed(2);
+    sumaDivide = ((parseFloat(restaPriceEntrance) + parseFloat(sumaTotal)) / parseFloat(Months)).toFixed(2);
 
-restaPriceEntrance = (parseFloat(Price)- parseFloat(Entrance));
-
-sumaTotal =( ((parseFloat(Price)-parseFloat(Entrance))*tae) ).toFixed(2);
-
-
-console.log(sumaTotal);
-
-console.log('lo de abajo es lo de divide')
-
-sumaDivide = ( ( parseFloat(restaPriceEntrance) + parseFloat(sumaTotal) ) / parseFloat(Months) ).toFixed(2);
-
-
-console.log(sumaDivide);
-console.log(sumaTotal);
-
-if(isNaN(sumaTotal)){
-sumaTotal= '';
-}
-
-if(isNaN(sumaDivide)){
-    sumaDivide= '';
+    if (isNaN(sumaTotal)) {
+        sumaTotal = '';
     }
 
-/* 
-if(Price==''||Entrance==''||Months==''){
-alert('Rellena todos los campos')
-}
-
-if((Price<0||Price>90000)||(Entrance<0||Entrance>90000)||(Months<0||Months>90000)){
-alert('Los numeros tienes que estar entre el 0 y el 90000');
-return '';
-}
-*/
+    if (isNaN(sumaDivide)) {
+        sumaDivide = '';
+    }
 
 
+    if (Price === "" || Entrance === "" || Months === "") {
+        document.getElementById("demo").innerHTML = "All fields are mandatory";
+        return false
+    }
+    else {
+        document.getElementById("demo").innerHTML = "";
 
+        if (cont == 1) {
 
-
-
-
-//esto es una prueba 
-
-
-        if ( Price ==="" || Entrance ==="" || Months ==="" ) { 
-            document.getElementById("demo").innerHTML = "All fields are mandatory";
-            return false
+            document.getElementById("resultadoFinal").innerHTML = `${sumaDivide}€`;
+            document.getElementById("Prize").innerHTML = `${Price}€`;
+            document.getElementById("Entrance").innerHTML = `${Entrance}€`;
+            document.getElementById("Months").innerHTML = `${Months}`;
         }
-        else { 
-            document.getElementById("demo").innerHTML = "";
-           
-            
-            if(cont==1){
 
-                document.getElementById("resultadoFinal").innerHTML = `${sumaDivide}€`;
-                document.getElementById("Prize").innerHTML= `${Price}€`;
-                document.getElementById("Entrance").innerHTML =  `${Entrance}€`;
-                document.getElementById("Months").innerHTML = `${Months}`;
-                
-            }
+        return false
+    }
 
-            return false
-        }
-        
 
 }
-    
-    
+
+
+function myClean(Price, Entrance, Months, tae, cont) {
+
+ 
+
+    if (cont == 2) {
+
+        document.getElementById("resultadoFinal").innerHTML = '';
+        document.getElementById("Prize").innerHTML = '';
+        document.getElementById("Entrance").innerHTML = '';
+        document.getElementById("Months").innerHTML = '';
+    }
+
+
+
+}
+
